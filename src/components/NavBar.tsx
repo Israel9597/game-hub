92369d8330c058a2eb52ee2228d028c7d93a107f
Israel Haile <israelbelete@gmail.com>
2024-07-20 17:40:48 -0400

    title update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,12 @@
-import { HStack, Image, Spacer, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import DarkModeToggle from "./DarkModeToggle";
 import Search from "./Search";
-import { useState } from "react";
 
 interface Props {
   onSearch: (search: string) => void;
 }
 const NavBar = ({ onSearch }: Props) => {
-  const [search, setSearch] = useState();
   return (
     <HStack justifyContent="space-between">
       <Image boxSize="60px" src={logo} />
